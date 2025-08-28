ddc88e191c05ff1c600ebd923c851f282a01d904
Shinnn2374 <168295007+LeftEye2374@users.noreply.github.com>
2025-08-28 16:54:52 +0300

    ...

--- simple-application/src/main/java/com/example/simple_application/SimpleApplication.java
+++ simple-application/src/main/java/com/example/simple_application/SimpleApplication.java
@@ -2,12 +2,19 @@ package com.example.simple_application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
 
 @SpringBootApplication
+@RestController
+@RequestMapping(value = "hello")
 public class SimpleApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SimpleApplication.class, args);
 	}
 
+	@GetMapping("/{firstName}")
+	public String helloGET(@PathVariable String firstName, @RequestParam String lastName) {
+		return String.format("{\"message\":\"Hello %s %s\"}",
+				firstName, lastName);
+	}
 }
