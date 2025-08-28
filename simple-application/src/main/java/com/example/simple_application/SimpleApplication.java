25bb1b324ee39349c43274cfac83cfb210c8a4f9
Shinnn2374 <168295007+LeftEye2374@users.noreply.github.com>
2025-08-28 17:55:40 +0300

    ...

--- simple-application/src/main/java/com/example/simple_application/SimpleApplication.java
+++ simple-application/src/main/java/com/example/simple_application/SimpleApplication.java
@@ -2,14 +2,28 @@ package com.example.simple_application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @RestController
 @RequestMapping(value = "hello")
+@EnableDiscoveryClient
 public class SimpleApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SimpleApplication.class, args);
+	}
+
+	public String helloRemoteServiceCall(String firstName, String lastName) {
+		RestTemplate restTemplate = new RestTemplate();
+		ResponseEntity<String> restExchange =
+				restTemplate.exchange(
+						"http://logical-service-id/name/" + "{firstName}/{lastName}", HttpMethod.GET, null, String.class,
+						firstName, lastName);
+		return restExchange.getBody();
 	}
 
 	@GetMapping("/{firstName}")
