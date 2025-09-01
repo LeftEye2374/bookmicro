a5f824974edcaeae34c8c93880652c7032fd2c67
Shinnn2374 <168295007+LeftEye2374@users.noreply.github.com>
2025-09-01 16:58:00 +0300

    ...

--- License-Service/src/main/java/com/example/License/Service/LicenseServiceApplication.java
+++ License-Service/src/main/java/com/example/License/Service/LicenseServiceApplication.java
@@ -2,6 +2,11 @@ package com.example.License.Service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
 
 @SpringBootApplication
 public class LicenseServiceApplication {
@@ -10,4 +15,19 @@ public class LicenseServiceApplication {
 		SpringApplication.run(LicenseServiceApplication.class, args);
 	}
 
+	@Bean
+	public SessionLocaleResolver localResolver(){
+		SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+		localeResolver.setDefaultLocale(Locale.US);
+		return localeResolver;
+	}
+
+	@Bean
+	public ResourceBundleMessageSource messageSource(){
+		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+		messageSource.setUseCodeAsDefaultMessage(true);
+
+		messageSource.setBasename("messages");
+		return messageSource;
+	}
 }
