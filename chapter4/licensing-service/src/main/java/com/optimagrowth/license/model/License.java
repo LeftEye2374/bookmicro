7c7abf20d0656539fa6793247f73eee2cdf57c69
Shinnn2374 <168295007+LeftEye2374@users.noreply.github.com>
2025-09-09 16:50:33 +0300

    ...

--- chapter4/licensing-service/src/main/java/com/optimagrowth/license/model/License.java
+++ chapter4/licensing-service/src/main/java/com/optimagrowth/license/model/License.java
@@ -7,7 +7,9 @@ import lombok.Setter;
 import lombok.ToString;
 
 
-@Getter @Setter @ToString
+@Getter
+@Setter
+@ToString
 public class License extends RepresentationModel<License> {
 
 	private int id;
@@ -17,4 +19,53 @@ public class License extends RepresentationModel<License> {
 	private String productName;
 	private String licenseType;
 
+
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getLicenseId() {
+		return licenseId;
+	}
+
+	public void setLicenseId(String licenseId) {
+		this.licenseId = licenseId;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getOrganizationId() {
+		return organizationId;
+	}
+
+	public void setOrganizationId(String organizationId) {
+		this.organizationId = organizationId;
+	}
+
+	public String getProductName() {
+		return productName;
+	}
+
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+
+	public String getLicenseType() {
+		return licenseType;
+	}
+
+	public void setLicenseType(String licenseType) {
+		this.licenseType = licenseType;
+	}
 }
